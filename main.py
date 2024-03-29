ea1db516105b559716d5c85f0181c058cad9c7f3
GmeX1 <gmex51@yandex.ru>
2024-03-29 15:32:13 +0300

    Revert "Сделал базовую базу для авторизации"
    This reverts commit 6f506f23a7c8e425343deb7a10bb819ea1d95284.

--- main.py
+++
@@ -1,8 +0,0 @@
-from temp_vars import API_ID, API_HASH
-from pyrogram import Client
-
-client = Client('test_client', api_id=API_ID, api_hash=API_HASH)
-
-client.start()
-client.send_message('me', 'Test message!')
-client.stop()

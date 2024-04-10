a389226b43e48b825f5cca222fc98b12d90df232
Ivan <ivangrekov83@gmail.com>
2024-04-10 23:27:33 +0300

    Были дописаны настройки, появился прототип белого списка

--- app/handlers.py
+++ app/handlers.py
@@ -1,17 +1,18 @@
-from aiogram import F, Router, types
+from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message, CallbackQuery
-import app.key as k
-from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
+import app.key as k
 from Private import api_id, api_hash
 from pyrogram import Client
+from temp_vars import BUY_MAX_LVL, BUY_CLICK, BUY_MINER, BUY_ENERGY
 from pyrogram.errors.exceptions import bad_request_400
 
 router = Router()
 
 
-# TODO: Дописать настройки кликера, словарь
+# TODO: Дописать настройки кликера, БД, отчет ошибок
 
 class Reg(StatesGroup):
     number = State()
@@ -20,9 +21,31 @@ class Reg(StatesGroup):
     Clients = State()
 
 
+class Max(StatesGroup):
+    max_ = State()
+
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.reply(f'Привет. \nТвой ID:{message.from_user.id} ты есть в нашей системе', reply_markup=k.main)
+    with open('white_liste.txt') as file:
+        white_liste = [str(i.replace('\n', '')) for i in file.readlines()]
+
+    if str(message.from_user.id) not in white_liste:
+        swoi = message.text[7:]
+        print(white_liste, message.from_user.id, swoi)
+        if str(swoi) in white_liste:
+            if str(swoi) != str(message.from_user.id):
+                with open('white_liste.txt', 'w') as file:
+                    file.write(swoi + '\n')
+                    file.write(str(message.from_user.id))
+                    await message.reply(f'Привет. \nТвой ID:{message.from_user.id} ты есть в нашей системе.\n'
+                                        f'Тебе осталось зарегистрироваться по команде /reg', reply_markup=k.main)
+            else:
+                await message.answer(
+                    'По собственной ссылке регистрироваться нельзя. Или вы не зарегистрированы в системе')
+    else:
+        await message.reply(f'Привет. \nТвой ID:{message.from_user.id} ты есть в нашей системе.\n'
+                            f'Тебе осталось зарегистрироваться по команде /reg', reply_markup=k.main)
 
 
 @router.message(Command('help'))
@@ -30,12 +53,12 @@ async def get_help(message: Message):
     await message.answer(f'/help - показывает функции бота'
                          f'\n "Профиль" - показывает сколько намайнил кликер'
                          f'\n /start - запускает бота'
-                         f'\n "Запустить кликер на арбузы🍉" - Запускает кликер  ')
+                         f'\n "Запустить кликер наарбузы🍉" - Запускает кликер  ')
 
 
 @router.message(F.text == '🆘Помощь')
 async def get_prof(message: Message):
-    await message.answer('Можете задать вопрос админу или сообщить ему об ошибке - ....')
+    await message.answer('Можете задать вопрос админу или сообщить ему об ошибке - @Mr_Mangex')
 
 
 @router.message(F.text == '👤Профиль')
@@ -92,3 +115,91 @@ async def reg_kod(message: Message, state: FSMContext):
         await state.clear()
     except bad_request_400:
         await message.answer('Ошибка входа. Отправьте контакт заново и перечитайте условия')
+
+
+# @router.message(F.text == '🤝Поделиться с другом')
+# async def get_ref(message: Message):
+#     await message.answer(f'Вот ваша реферальная ссылка: https://t.me/ClickManagerbot?start={message.from_user.id}')
+
+
+@router.message(F.text == '⚙️Настройки кликера')
+async def setr_klik(message: Message):
+    await message.reply('Выберите действие: ', reply_markup=k.Settings)
+
+
+@router.callback_query(F.data == 'Klik')
+async def klik(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_text('Включить или выключить автоматическую покупку кликов', reply_markup=k.Yes_or_No_K)
+
+
+@router.callback_query(F.data == 'YesK')
+async def YesK(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'NoK')
+async def NoK(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'Avto_klik')
+async def Avto_klik(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_text('Включить или выключить автоматическую покупку Авто кликов',
+                                     reply_markup=k.Yes_or_No_A)
+
+
+@router.callback_query(F.data == 'YesA')
+async def YesA(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'NoA')
+async def NoA(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'Energy')
+async def Energy(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.edit_text('Включить или выключить автоматическую покупку энергии"',
+                                     reply_markup=k.Yes_or_No_E)
+
+
+@router.callback_query(F.data == 'YesE')
+async def YesE(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'NoE')
+async def NoE(callback: CallbackQuery):
+    await callback.answer('Опперация была успешно выполнена')
+    await callback.message.edit_text('Выберите действие: ', reply_markup=k.Settings)
+    pass
+
+
+@router.callback_query(F.data == 'Max_lvl')
+async def klik(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await callback.message.answer('Напишите какой лимит хотите поставить (цифра)')
+    await state.set_state(Max.max_)
+
+
+@router.message(Max.max_)
+async def max_message(message: Message, state: FSMContext):
+    try:
+        await state.update_data(max_=int(message.text))
+        await message.answer('Опперация была успешно выполнена✅')
+    except ValueError:
+        await state.set_state(Max.max_)

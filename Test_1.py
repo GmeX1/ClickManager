07d72f3e14c2363f0f109d2283ded870d1758a6b
GmeX1 <gmex51@yandex.ru>
2024-04-15 19:00:44 +0300

    Добавил TODO

--- Test_1.py
+++ Test_1.py
@@ -4,12 +4,14 @@ from aiogram import Bot, Dispatcher
 from loguru import logger
 from Private import TOKEN
 from app.handlers import router
-from db.functions import init, db_add_user
+from db.functions import init
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
+# Одноразовое приглашение с комментарием (проверка user_id)
+# TODO: Использовать md5 для реферальной системы
 async def main():
     await init()
     dp.include_router(router)
